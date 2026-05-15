57b3ca68113f171d0f8720b45cafa919bdefa16d
Lucas <lucasmt5566@gmail.com>
2026-05-15 12:55:38 -0300

    implementando selector com recoil

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+
+import App from "./App";
+
 import { RecoilRoot } from "recoil";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RecoilRoot>
-    <App />
-  </RecoilRoot>
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </React.StrictMode>
 );
